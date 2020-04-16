7b2ce8d053274fe81b1a5b016d3c2cbd7fc07d51
Karl Jensen <karljensen@gmail.com>
2020-04-16 12:00:23 -0700

    Do not do timezone conversion on api server

--- server/src/db/connect.js
+++ server/src/db/connect.js
@@ -1,6 +1,14 @@
 const pg = require("pg");
 const client = new pg.Client();
 
+/*
+  Overwrite the parser for timestamp,
+  this way it does not use new Date,
+  and covert from UTC to local on
+  the server.
+*/
+pg.types.setTypeParser(1114, (stringValue) => stringValue);
+
 (async function () {
   try {
     await client.connect();
